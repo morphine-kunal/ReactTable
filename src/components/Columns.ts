e0653b3f7b155db035e6bc48a3f4f287cf670165
Kunal Kishore <kunalkishore1807@gmail.com>
2023-05-15 15:26:55 +0530

    🎉 feat(BasicTable.jsx): add BasicTable component 🎉 feat(Columns.ts): add COLUMNS constant The BasicTable component is added to display a basic table with data from the MOCK_DATA.json file. The COLUMNS constant is added to define the columns of the table. This improves the reusability of the table component as it can be used to display different data by just changing the columns.

---
+++ src/components/Columns.ts
@@ -0,0 +1,23 @@
+export const COLUMNS = [
+    {
+      Header: "ID",
+      accessor: "id",
+    },
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "Status",
+      accessor: "status",
+    },
+    {
+      Header: "Role",
+      accessor: "role",
+    },
+    {
+      Header: "Last Login",
+      accessor: "last_login",
+    },
+  ];
+  
