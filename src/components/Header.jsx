a7d8ec8df427f16e599cacfd9bb421547d5d2723
Kunal Kishore <kunalkishore1807@gmail.com>
2023-05-15 15:27:10 +0530

    ✨ feat(Header.jsx): add Header component ✨ feat(table.css): add table styles The Header component is added to the application to display the company settings. The component contains buttons that allow the user to navigate to different settings pages. The table.css file is added to the application to style the tables used in the application. The styles include font-family, border-collapse, background-color, and padding.

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <h2>Company Settings</h2>
+      </div>
+      <div>
+        <button>General</button>
+        <button>User</button>
+        <button>Plan</button>
+        <button>Billing</button>
+        <button>Integrations</button>
+      </div>
+    </>
+  );
+};
+
+export default Header;
